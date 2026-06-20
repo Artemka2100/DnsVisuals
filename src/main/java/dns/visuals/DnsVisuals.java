683a04461bcd1a95c5f9859896995ce54e722a12
Icekech <142043949+Artemka2100@users.noreply.github.com>
2026-06-20 09:38:48 +0300

    Add mod entrypoint

---
+++ src/main/java/dns/visuals/DnsVisuals.java
@@ -0,0 +1,55 @@
+package dns.visuals;
+
+import dns.visuals.config.ConfigManager;
+import dns.visuals.gui.ClickGuiScreen;
+import dns.visuals.hud.HudManager;
+import dns.visuals.module.Module;
+import dns.visuals.module.ModuleManager;
+import dns.visuals.util.CpsTracker;
+import dns.visuals.util.TpsTracker;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+/** Mod entrypoint. Wires up modules, HUD, config, CPS/TPS tracking and the GUI open key. */
+public class DnsVisuals implements ClientModInitializer {
+	public static final String MOD_ID = "dnsvisuals";
+	public static final CpsTracker CPS = new CpsTracker();
+	public static final TpsTracker TPS = new TpsTracker();
+
+	private boolean openKeyWasDown = false;
+
+	@Override
+	public void onInitializeClient() {
+		ModuleManager.INSTANCE.init();
+		ConfigManager.load();
+		HudManager.register();
+
+		ClientTickEvents.END_CLIENT_TICK.register(this::onClientTick);
+		ClientLifecycleEvents.CLIENT_STOPPING.register(client -> ConfigManager.save());
+
+		System.out.println("[DnsVisuals] Loaded " + ModuleManager.INSTANCE.all().size() + " modules.");
+	}
+
+	private void onClientTick(MinecraftClient mc) {
+		// CPS: drain queued click presses (accurate, not sampled)
+		while (mc.options.attackKey.wasPressed()) CPS.onLeftClick();
+		while (mc.options.useKey.wasPressed()) CPS.onRightClick();
+
+		// TPS: approximate by feeding a tick each client tick while in a world
+		if (mc.world != null) TPS.onTimeUpdate(1);
+
+		// Open ClickGUI on the configured key (default Right Shift)
+		Module clickGui = ModuleManager.INSTANCE.find("ClickGUI");
+		int key = clickGui != null ? clickGui.keyVal("Open key") : GLFW.GLFW_KEY_RIGHT_SHIFT;
+		boolean down = key != GLFW.GLFW_KEY_UNKNOWN
+				&& InputUtil.isKeyPressed(mc.getWindow().getHandle(), key);
+		if (down && !openKeyWasDown && mc.currentScreen == null) {
+			mc.setScreen(new ClickGuiScreen());
+		}
+		openKeyWasDown = down;
+	}
+}
